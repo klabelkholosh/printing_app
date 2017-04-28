ddb82455937c674c471fc5f783dc68423e1043c3
smpmbatha <smpmbatha@gmail.com>
2017-04-28 13:34:42 +0200

    balbala\

--- src/AppBundle/Form/MachineType.php
+++ src/AppBundle/Form/MachineType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-
+//test
 class MachineType extends AbstractType
 {
     /**
