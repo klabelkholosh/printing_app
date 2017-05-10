05345b63761fcd97db24aab44bdfd9028ae1e126
klabelkholosh <klabelkholosh@gmail.com>
2017-05-10 10:04:42 +0200

    trying to fix api-platform config files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-	        //new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
+	    new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
             new AppBundle\AppBundle(),
         ];
 
